df10b62e7d95a224de65688478ae51d43d95a0bd
EvanR <evanrinehart@gmail.com>
2015-03-11 18:25:30 -0500

    fix a bug in E monoid
    I realized that using race will cause some events to be lost if the
    thread is killed after atomically dequeuing. Now it deterministically
    tries the left event then the right event.

--- Core.hs
+++ Core.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 module Main where
 
 import Control.Applicative
@@ -6,6 +7,7 @@ import Data.Monoid
 import Control.Concurrent
 import System.IO
 import System.Exit
+import Data.Time
 
 import Demo
 import Control.Broccoli
@@ -29,29 +31,44 @@ main = do
   runProgram $ do
     (onBoot, boot)    <- newE 
     (mouse, setMouse) <- newX (0,0)
-    (onClick, click)  <- newE :: Setup (E MouseButton, MouseButton -> IO ())
+    (onClick, click)  <- newE
     (window, resize)  <- newX dim0
-    (onQuit, quit)    <- newE :: Setup (E (), () -> IO ())
+    (onQuit, quit)    <- newE
+    (time, tick)      <- newE
     input (inputWorker eventInH setMouse click resize quit)
-    let (picture, sound) = program mouse onClick window onBoot
-    output sound play
+    input (forever (threadDelay 1000000 >> getCurrentTime >>= tick))
+    let (picture, sound) = program mouse onClick window onBoot time
+    --output sound play
+    --output sound print
+    --output time print
     output picture paint
     return (boot (), onQuit)
+
+mkay :: Either UTCTime Int -> Int -> Int
+mkay (Left _) n = (n+1) `mod` 4
+mkay (Right m) _ = m
 
 program :: X R2
         -> E MouseButton
         -> X R2
         -> E ()
+        -> E UTCTime
         -> (E [Paint], E Note)
-program mouse click window boot = (picture, sound) where
+program mouse click window boot time = (picture, sound) where
+  timeN = debugX $ accumulate (time <||> override) 0 mkay
+  override = (\x -> (x+2) `mod` 4) <$> snapshot_ click timeN
   sound = note <$> select              :: E Note
-  select = snapshot_ click hover       :: E Int
+  ships = accumulate sound 0 (+)
+  offset = (,0) . realToFrac <$> ships
+  --select = snapshot_ click hover       :: E Int
+  select = never
   hover = liftA2 overQuad mouse chart  :: X Int
   chart = fmap fromLayout rects        :: X (Chart R2 Int)
-  rects = liftA2 layout zero window    :: X [(Rect Double, Int)]
+  rects = liftA2 layout offset window  :: X [(Rect Double, Int)]
   picture = view simon simonView       :: E [Paint]
   simon = liftA2 Simon selection rects :: X Simon
-  selection = accumulate select Nothing (\n _ -> Just n) :: X (Maybe Int)
+  --selection = accumulate select Nothing (\n _ -> Just n) :: X (Maybe Int)
+  selection = Just <$> timeN
   view = repainter boot
   zero = pure (0,0) :: X (Double, Double)
 
